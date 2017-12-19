e4398da1a1281b63f777a4c169ce7b0f2c026831
Manxetan <Manxetan92@gmail.com>
2017-12-20 02:02:38 +0500

    1

---
+++ Bot Application3/Controllers/MessagesController.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application3
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+               
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+               
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+               
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Bot Application3/Dialogs/RootDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application3.Dialogs
+{
+    [Serializable]
+
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+  
+            var activity = await result as Activity;
+
+            string UserName = (activity.From.Name);         
+                await context.PostAsync($"Привет {UserName}. ");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}  
